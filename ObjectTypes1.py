6b0bdf843a4b25f043573f6b138e544b9031b885
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2021-08-16 23:54:53 +0530

    Update ObjectTypes1.py

--- ObjectTypes1.py
+++ ObjectTypes1.py
@@ -4,3 +4,18 @@ print(1+2) #addition
 print(1-2) #Substract
 print(1*2) #Multiplication
 print('100**2') #Exponentation
+
+a = 1
+b = 12
+c = a+b
+print(a+b)
+print(a-b)
+print(len(c))
+
+import math
+math.sqrt(a)
+math.pi(3.14)
+
+import random
+random.random()
+
