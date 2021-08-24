983e79a001b6975329a38403bca7b6294495272a
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2021-08-24 12:52:33 +0530

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,48 @@
+a = 12
+b = 13
+# Expression operator
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a>>b)
+print(a**b)
+print(a&b)
+
+# Built-in mathematical functions
+'''pow,abs,round,int,hex,bin'''
+# Utility modules
+'''random,math etc.'''
+
+# python Expression operator
+
+'''
+yield x                     Generator function send protocol
+lambda args:expression      Anonymous function generation
+x if y else z               Ternary selection
+x or y                      Logical OR
+x and y                     LOgical AND
+not x                       Logical negation
+x in y,x not in y           Membership(iterables,sets)
+x is y,x not y              Object Indentity tests
+x<y,x<=y,x>y,x>=y,x==y,x!=y  Magnitude comparsion
+x bitwise OR y               Bitwise OR
+x^y                          Bitwise XOR
+x&y                          Bitwise AND
+x<<y,x>>y                    Shift x left or right by y bitwise
+x+y                          Addition,concatenation
+x-y                          Subtraction,set difference
+x*y                          Multiplication,repetition
+x%y                          Remainder,format
+x/y,x//y                     Division
+-x,+x                        Negation,Indentity
+~X                           Bitwise NOT
+X**Y                         Power(exponentiation)
+x[i]                         Indexing(sequence,mapping,others)
+x[i:i:k]                     Slicing
+x(...)                       call(function,method,class)
+x.attr                       Attribute referance
+(...)                        Tuple,expression,generator Expression
+[...]                        List,list comprehension
+{...}                        Dictionary,set,ser and dictionary comprehensions
+'''
